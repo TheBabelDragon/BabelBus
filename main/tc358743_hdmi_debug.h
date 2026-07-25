1a5a6187230d244956f50c91516f71a4bc455350
TheBabelDragon <sphg5nyvyr@privaterelay.appleid.com>
2026-07-24 21:24:18 -0600

    SYS_STATUS: log all Linux bits (SCDT/PLL/TMDS/HDMI/SYNC)

--- main/tc358743_hdmi_debug.h
+++ main/tc358743_hdmi_debug.h
@@ -4,6 +4,11 @@
 #include "esp_log.h"
 #include "tc358743.h"
 
+/**
+ * Linux tc358743_regs.h SYS_STATUS (0x8520):
+ *  bit0 DDC5V  bit1 TMDS  bit2 PHY_PLL  bit3 PHY_SCDT
+ *  bit4 HDMI   bit5 HDCP  bit6 AVMUTE  bit7 SYNC
+ */
 static inline void tc358743_debug_status(tc358743_t *dev)
 {
     if (!dev) {
@@ -13,6 +18,8 @@ static inline void tc358743_debug_status(tc358743_t *dev)
     if (tc358743_sys_status(dev, &st) != ESP_OK) {
         return;
     }
-    ESP_LOGI("tc358743", "SYS_STATUS=0x%02x (TMDS=%d HDMI=%d SYNC=%d DDC5V=%d)", st, (int)(st >> 1) & 1,
-             (int)(st >> 4) & 1, (int)(st >> 7) & 1, (int)st & 1);
+    ESP_LOGI("tc358743",
+             "SYS_STATUS=0x%02x DDC5V=%d TMDS=%d PLL=%d SCDT=%d HDMI=%d HDCP=%d AVMUTE=%d SYNC=%d",
+             st, (int)(st & 1), (int)((st >> 1) & 1), (int)((st >> 2) & 1), (int)((st >> 3) & 1),
+             (int)((st >> 4) & 1), (int)((st >> 5) & 1), (int)((st >> 6) & 1), (int)((st >> 7) & 1));
 }
